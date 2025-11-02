8b0b4ce74ae9e96534f0823dc8976f6d9b3543b6
Muresan Claudiu <muresanclaudiu0707@gmail.com>
2025-11-02 22:31:09 +0200

    bluetooth connection works

---
+++ app/src/main/java/com/example/carcontroller/ConnectThread.java
@@ -0,0 +1,74 @@
+package com.example.carcontroller;
+
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.util.Log;
+
+import androidx.core.content.ContextCompat;
+
+import java.io.IOException;
+import java.util.UUID;
+
+public class ConnectThread extends Thread {
+    private static final String TAG = "ConnectThread";
+
+    private final BluetoothSocket targetSocket;
+    private final BluetoothDevice targetDevice;
+    private final BluetoothManager bluetoothManager;
+    private final BluetoothAdapter bluetoothAdapter;
+    private final Context context;
+
+    public ConnectThread(BluetoothDevice device, BluetoothManager manager, Context cntx) {
+        targetDevice = device;
+        bluetoothManager = manager;
+        bluetoothAdapter = bluetoothManager.getAdapter();
+        BluetoothSocket tmp = null;
+        context = cntx;
+
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED) {
+            try {
+                tmp = targetDevice.createRfcommSocketToServiceRecord(UUID.fromString("00000000-0000-0000-0000-000000000000"));
+            } catch (IOException e) {
+                Log.e(TAG, "Socket's create() method failed", e);
+            }
+        } else {
+            Log.e(TAG, "Missing BLUETOOTH_CONNECT permission");
+        }
+        targetSocket = tmp;
+    }
+
+    @Override
+    public void run() {
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            Log.e(TAG, "Missing BLUETOOTH_CONNECT permission");
+            return;
+        }
+
+        bluetoothAdapter.cancelDiscovery();
+
+        try {
+            targetSocket.connect();
+            Log.i(TAG, "Connection successful!");
+        } catch (IOException e) {
+            Log.e(TAG, "Could not connect; closing socket", e);
+            try {
+                targetSocket.close();
+            } catch (IOException e2) {
+                Log.e(TAG, "Could not close the client socket", e2);
+            }
+        }
+    }
+
+    public void cancel() {
+        try {
+            targetSocket.close();
+        } catch (IOException e) {
+            Log.e(TAG, "Could not close the client socket", e);
+        }
+    }
+}
--- app/src/main/java/com/example/carcontroller/MainActivity.java
+++ app/src/main/java/com/example/carcontroller/MainActivity.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.ParcelUuid;
 import android.util.*;
 import android.view.*;
 import android.widget.*;
@@ -29,8 +30,6 @@ import androidx.core.app.ActivityCompat;
 */
 
 public class MainActivity extends AppCompatActivity {
-
-
     ActivityResultLauncher<Intent> launchData = null;
     BluetoothManager myBluetoothManager = null;
     BluetoothAdapter myBluetoothAdapter = null;
@@ -38,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<String> myBluetoothDevicesNames = null;
     ArrayAdapter<String> devicesAdapter = null;
     ListView lvNewDevice;
+    BluetoothDevice selectedDevice;
 
 
     @Override
@@ -71,19 +71,31 @@ public class MainActivity extends AppCompatActivity {
                 });
 
 
-        //Crashes the app but it detects the selections of the app
+        // Wait for user to select a bluetooth device to connect to
         lvNewDevice.setOnItemClickListener((parent, view, position, id) -> {
             String btDeviceName = lvNewDevice.getItemAtPosition(position).toString();
-            /*for(int i = 0; i <= myBluetoothDevices.size(); i++){
-                if (checkSelfPermission(Manifest.permission.BLUETOOTH_CONNECT)!= PackageManager.PERMISSION_GRANTED) {
-                    requestPermissions(new String[]{Manifest.permission.BLUETOOTH_CONNECT}, 1);
-                }
+            if (checkSelfPermission(Manifest.permission.BLUETOOTH_CONNECT)!= PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.BLUETOOTH_CONNECT}, 1);
+            }
+            for(int i = 0; i < myBluetoothDevices.size(); i++){
                 if (Objects.equals(myBluetoothDevices.get(i).getName(), btDeviceName)){
-                    //((TextView) findViewById(R.id.textView2)).setText(myBluetoothDevices.get(i).getName());
-
+                    selectedDevice = myBluetoothDevices.get(i);
+                    ((TextView) findViewById(R.id.textView2)).setText(selectedDevice.getName());
+//                    for (ParcelUuid uuid : selectedDevice.getUuids()) {
+//                        Log.d("BT", "Device UUID: " + uuid.toString());
+//                    }
+                    if (selectedDevice.getBondState() != BluetoothDevice.BOND_BONDED) {
+                        Log.e("Bluetitties", "Device not paired!");
+                        selectedDevice.createBond();
+                        return;
+                    }
+                    myBluetoothAdapter.cancelDiscovery();
+                    ConnectThread newConnectThread = new ConnectThread(selectedDevice, myBluetoothManager,this);
+                    newConnectThread.start();
                 }
-            }*/
+            }
         });
+
     }
 
     @Override
@@ -146,7 +158,6 @@ public class MainActivity extends AppCompatActivity {
             Log.d("BluetoothOperations", "Bluetooth already enabled");
         }
     }
-
     public void startDiscoveryDevices(View v){
         // Permissions again because Android thinks that I want to spy on ..... me
         if(checkSelfPermission(Manifest.permission.BLUETOOTH_SCAN)!=PackageManager.PERMISSION_GRANTED)
@@ -163,35 +174,15 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
     public void handleDriverNameText (View v){
         String driverName = ((EditText) findViewById(R.id.driverTextBoxID)).getText().toString();
         ((TextView) findViewById(R.id.inputName)).setText(driverName);
         Toast.makeText(this, "Driver introduced", Toast.LENGTH_LONG).show(); //I can add alert if new driver is added to the DB or if he already exists
         Log.d("Driver name", driverName);
     }
-
     public void launchActivityStatistics(View v){
         Intent i = new Intent(this, StatisticsActivity.class);
         startActivity(i);
-    }
-
-
-    private void deleteLater(View v, String name, int i){
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.BLUETOOTH_CONNECT}, 1);
-            return;
-        }
-    }
-    private void checkPermissions(int permissionCode){
-        if (permissionCode == 1){
-            if (checkSelfPermission(Manifest.permission.BLUETOOTH_CONNECT)!= PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(new String[]{Manifest.permission.BLUETOOTH_CONNECT}, 1);
-            }
-        }
-        if(permissionCode == 2){
-            if(checkSelfPermission(Manifest.permission.BLUETOOTH_SCAN)!=PackageManager.PERMISSION_GRANTED)
-                requestPermissions(new String[]{Manifest.permission.BLUETOOTH_SCAN}, 1);
-
-        }
     }
 }
