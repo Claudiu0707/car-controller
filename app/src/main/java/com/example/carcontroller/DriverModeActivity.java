cb1a3be907b4cd73267889beb57087f9367f7b48
Muresan Claudiu <muresanclaudiu0707@gmail.com>
2025-12-28 02:53:03 +0200

    all basic controls created

--- app/src/main/java/com/example/carcontroller/DriverModeActivity.java
+++ app/src/main/java/com/example/carcontroller/DriverModeActivity.java
@@ -1,8 +1,12 @@
 package com.example.carcontroller;
 
+import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,11 +16,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 import java.util.List;
 
+
 public class DriverModeActivity extends AppCompatActivity {
     private static final String TAG = "DriverModeActivityTAG";
     DevicesConnected devicesConnected = DevicesConnected.getInstance();
-    BluetoothDevice car = null;
+    BluetoothDevice carDevice = null;
 
+    Button backButton;
+
+    Button forwardButton, reverseButton, steerLeftButton, steerRightButton;
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,20 +39,69 @@ public class DriverModeActivity extends AppCompatActivity {
         });
 
         if (!devicesConnected.getDevices().isEmpty()) {
-            car = devicesConnected.getDevices().get(0);
+            carDevice = devicesConnected.getDevices().get(0);
         }
-    }
 
-
-   /* public void sendData () {
         List<BluetoothDevice> deviceList = devicesConnected.getDevices();
         BluetoothService service = new BluetoothService();
-        if (!deviceList.isEmpty()) {
-            for (BluetoothDevice device: deviceList){
-                BluetoothSocket socket = devicesConnected.getSocket(device);
-                service.initializeStream(socket);
+        BluetoothSocket socket = devicesConnected.getSocket(carDevice);
+        service.initializeStream(socket);
+
+        // Buttons initialization
+        backButton = (Button) findViewById(R.id.backButton2);
+
+        forwardButton = (Button) findViewById(R.id.forwardButton);
+        reverseButton = (Button) findViewById(R.id.reverseButton);
+        steerLeftButton = (Button) findViewById(R.id.steerLeftButton);
+        steerRightButton = (Button) findViewById(R.id.steerRightButton);
+        // ---------------- BUTTON ONCLICK LISTENERS ----------------
+        backButton.setOnClickListener(v -> {
+            launchActivityMain();
+        });
+        forwardButton.setOnTouchListener((view, motionEvent) -> {
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
                 service.write("D1");
+                return true;
+            } else if (motionEvent.getAction() == MotionEvent.ACTION_UP || motionEvent.getAction() == MotionEvent.ACTION_CANCEL) {
+                service.write("D0");
+                return true;
             }
-        }
-    }*/
+            return false;
+        });
+        reverseButton.setOnTouchListener((view, motionEvent) -> {
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                service.write("D2");
+                return true;
+            } else if (motionEvent.getAction() == MotionEvent.ACTION_UP || motionEvent.getAction() == MotionEvent.ACTION_CANCEL) {
+                service.write("D0");
+                return true;
+            }
+            return false;
+        });
+        steerLeftButton.setOnTouchListener((view, motionEvent) -> {
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                service.write("D3");
+                return true;
+            } else if (motionEvent.getAction() == MotionEvent.ACTION_UP || motionEvent.getAction() == MotionEvent.ACTION_CANCEL) {
+                service.write("D0");
+                return true;
+            }
+            return false;
+        });
+        steerRightButton.setOnTouchListener((view, motionEvent) -> {
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                service.write("D4");
+                return true;
+            } else if (motionEvent.getAction() == MotionEvent.ACTION_UP || motionEvent.getAction() == MotionEvent.ACTION_CANCEL) {
+                service.write("D0");
+                return true;
+            }
+            return false;
+        });
+    }
+
+    private void launchActivityMain () {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/example/carcontroller/MainActivity.java
+++ app/src/main/java/com/example/carcontroller/MainActivity.java
@@ -6,8 +6,6 @@
 
 package com.example.carcontroller;
 
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.*;
@@ -21,12 +19,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity{
     private static final String TAG = "MainActivityTAG";
-
-    DevicesConnected devicesConnected = DevicesConnected.getInstance();
+    Button bluetoothButton, driverButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -41,33 +38,34 @@ public class MainActivity extends AppCompatActivity{
 
 
         // Buttons initialization
-        Button bluetoothButton = (Button) findViewById(R.id.bluetoothButton);
-        Button sendDataBUtton = (Button) findViewById(R.id.sendData);
+        bluetoothButton = (Button) findViewById(R.id.bluetoothButton);
+        driverButton = (Button) findViewById(R.id.driverButton);
+
         // ---------------- BUTTON ONCLICK LISTENERS ----------------
-        bluetoothButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(TAG, "onClick: bluetooth");
-                launchActivityBluetooth();
-            }
+        bluetoothButton.setOnClickListener(v -> {
+            Log.d(TAG, "onClick: bluetooth");
+            launchActivityBluetooth();
         });
-        sendDataBUtton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick (View view) {
-                // sendData();
-            }
+        driverButton.setOnClickListener(v -> {
+            Log.d(TAG, "onClick: driverMode");
+            launchActivityDriverMode();
         });
     }
 
-    public void handleDriverNameText (View v){
+    public void handleDriverNameText (View v) {
         String driverName = ((EditText) findViewById(R.id.driverTextBoxID)).getText().toString();
         ((TextView) findViewById(R.id.inputName)).setText(driverName);
         Toast.makeText(this, "Driver introduced", Toast.LENGTH_LONG).show();
         Log.d("Driver name", driverName);
     }
 
-    public void launchActivityBluetooth(){
+    public void launchActivityBluetooth () {
         Intent intent = new Intent(this, BluetoothManagerActivity.class);
+        startActivity(intent);
+    }
+
+    public void launchActivityDriverMode () {
+        Intent intent = new Intent(this, DriverModeActivity.class);
         startActivity(intent);
     }
 
